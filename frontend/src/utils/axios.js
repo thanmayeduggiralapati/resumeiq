4fb3d0582304795a001b7682696d95d3cb49467d
Thanmaye.D <thanmayeduggiralapati@gmail.com>
2026-05-24 15:29:17 +0530

    update API URL for production

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://resumeiq-c6d3.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
